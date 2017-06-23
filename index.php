3bebfe51b5d9e2fec09854718e86ae8971c673c8
Jerome BOUCHET <max95_@hotmail.Com>
2017-06-24 00:59:05 +0200

    changement de chris

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="description" content="DATAFIRM permet a ces utilisateurs de trouver une entreprise suivant son secteur d'activité. Il Géolicalise les visiteurs afin de leur permettre de voir les entreprises le plus proche d'eux.">
+    <meta name="keyboard" content="">
+
+    <title>DATAFIRM</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="slider/dist/css/bootstrap-slider.css" rel="stylesheet">
+
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <link href="magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/perso.css" rel="stylesheet">
+    <style>
+      img{
+         width:auto;
+          height: auto;
+   
+      }
+
+      .img-rounded{
+        width: 10%;
+        height: 10%;
+      }
+      .carousel-inner>.item>a>img, .carousel-inner>.item>img, .img-responsive, .thumbnail a>img, .thumbnail>img {
+            max-width: 100%;
+            height: auto;
+            display: inline;
+      }
+
+    </style>
+
+</head>
+
+<body id="page-top">
+
+  <?php include"menu.php" ?>
+
+  <div class="container marketing">
+
+    <!-- START THE FEATURETTES -->
+
+    <div class="jumbotron">
+            <h1>DATAFIRM</h1>
+            <p class="">En phase de TEST</p>
+            <p class="lead">Trouver facilement une entreprise en fonction de votre position et de son activité.</p>
+            <a class="btn btn-lg btn-success" href="recherche.php" role="button">Entrez</a>
+
+    </div>
+    <!-- jumbotron -->
+
+        <div class="row">
+          <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                      <div class="panel-heading" style="background-image: url(images/voilier.jpeg);"></div>
+                      <div class="panel-body text-center">
+                        <img class="panel-profile-img" src="images/jerome.jpeg">
+                        <h5 class="panel-title">Jerome Bouchet</h5>
+                        <p class="m-b">Apprenti Data Analyst.</p>
+                        <a href="https://www.linkedin.com/in/jerome-bouchet-375b28a2/" role="button " onclick="window.open(this.href); return false;"><img src="images\linkedin.jpeg" class="img-rounded" title="lien vers linkedin"></a> 
+                        <a href="https://fr.viadeo.com/fr/" role="button" onclick="window.open(this.href); return false;"><img src="images\viadeo.jpeg" class="img-rounded" title="lien vers viadeo"></a> 
+                        <a href="https://fr-fr.facebook.com/" role="button" onclick="window.open(this.href); return false;"><img src="images\fb.jpeg" class="img-rounded" title="lien vers facebook"></a>
+                      </div>
+                    </div>
+          </div>
+          <!--</div> col-md-4 m-b-lg -->
+          <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                      <div class="panel-heading" style="background-image: url(images/wallpaper_hugo1.jpeg);"></div>
+                      <div class="panel-body text-center">
+                        <img class="panel-profile-img" src="images/hugo.jpeg">
+                        <h5 class="panel-title">Hugo Fichot</h5>
+                        <p class="m-b">Apprenti Décisionnel.</p>
+                        <a href="https://www.linkedin.com/in/hugo-fichot-964322105/" role="button" onclick="window.open(this.href); return false;"><img src="images\linkedin.jpeg" class="img-rounded" title="lien vers linkedin"></a> 
+                        <a href="https://fr.viadeo.com/fr/" role="button" onclick="window.open(this.href); return false;"><img src="images\viadeo.jpeg" class="img-rounded" title="lien vers viadeo"></a> 
+                        <a href="https://fr-fr.facebook.com/" role="button" onclick="window.open(this.href); return false;"><img src="images\fb.jpeg" class="img-rounded" title="lien vers facebook"></a>
+                        </a>
+                      </div>
+                    </div>
+          </div>
+          <!--</div> col-md-4 m-b-lg -->
+          <div class="col-md-4 m-b-lg">
+                    <div class="panel panel-default panel-profile m-b-0">
+                      <div class="panel-heading" style="background-image: url(images/wallpaper_chris.jpeg);"></div>
+                      <div class="panel-body text-center">
+                        <img class="panel-profile-img" src="images/chris.jpeg">
+                        <h5 class="panel-title">Christopher Clemoux</h5>
+                        <p class="m-b">Apprenti Développeur.</p>
+                        <a href="https://www.linkedin.com/in/christopher-clemoux-830814145/" role="button" onclick="window.open(this.href); return false;"><img src="images\linkedin.jpeg" class="img-rounded" title="lien vers linkedin"></a> 
+                        <a href="https://fr.viadeo.com/fr/" role="button" onclick="window.open(this.href); return false;"><img src="images\viadeo.jpeg" class="img-rounded" title="lien vers viadeo"></a> 
+                        <a href="https://fr-fr.facebook.com/" role="button" onclick="window.open(this.href); return false;"><img src="images\fb.jpeg" class="img-rounded" title="lien vers facebook"></a>
+                      </div>
+                    </div>
+          </div>
+          <!--</div> col-md-4 m-b-lg -->
+
+        </div>
+        <!--</div> row -->
+</div class="container marketing">
+<!-- container marketing -->
+
+<!-- Footer-->
+<?php include"footer.php" ?>
+
+
+    <!-- jQuery -->
+    <script src="jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="slider/dist/bootstrap-slider.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
